bb893515f68b4a63a9052edafb9ad51703e42af9
Michael Schennum <mikeschenphoto@gmail.com>
2016-12-15 15:04:56 -0700

    adds bootstrap

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var Todo = (function () {
-    function Todo(task) {
-        this.task = task;
-    }
-    return Todo;
-}());
 function addTodo() {
     var input = document.getElementById("userInput");
     var storedInput = input.value;
-    var todo1 = new Todo(storedInput);
-    document.getElementById("results").innerHTML += "<li>" + todo1.task + "</li>";
-    clearForm();
+    if (storedInput === "") {
+        storedInput = "";
+        alert("Please Enter A Todo Item!");
+    }
+    else {
+        var todo1 = new MyApp.Todo(storedInput);
+        document.getElementById("results").innerHTML += "<li>" + todo1.task + "</li>";
+        clearForm();
+    }
 }
 function clearForm() {
     if (document.getElementById) {
--- app.ts
+++ app.ts
@@ -1,17 +1,14 @@
-class Todo {
-  task: string;
-
-  constructor(task: string) {
-    this.task = task;
-  }
-}
-
 function addTodo() {
   let input = <HTMLInputElement>document.getElementById("userInput");
   let storedInput = input.value;
-  let todo1 = new Todo(storedInput);
-  document.getElementById("results").innerHTML += "<li>" + todo1.task + "</li>";
-  clearForm();
+  if(storedInput === ""){
+    storedInput = "";
+    alert("Please Enter A Todo Item!")
+  } else {
+    let todo1 = new MyApp.Todo(storedInput);
+    document.getElementById("results").innerHTML += "<li>" + todo1.task + "</li>";
+    clearForm();
+  }
 }
 
 function clearForm() {
